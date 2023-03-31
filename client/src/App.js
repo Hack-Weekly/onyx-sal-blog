9b081fc8e8b704487931dc0b2d4ce7ab1867cc62
Jeffery Patterson <jpatterson933@ucla.edu>
2023-03-31 16:41:50 -0700

    1. Added basic nav bar to App.js --- No Styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>onyx sal blog</h1>
+      <nav>
+        <h1>Onyx Sal Blog</h1>
+        <ul>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Post</li>
+          <li>Metrics</li>
+        </ul>
+      </nav>
     </div>
   );
 }
