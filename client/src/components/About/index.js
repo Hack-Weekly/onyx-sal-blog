e7aa83801aea546c1c8276b770285ae8d6fab4ff
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-02 12:53:54 -0700

    1. Added unordered list of team members to about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,6 +4,15 @@ export const About = () => {
     return (
         <>
             <h1>Onyx Salamander About Page</h1>
+            <h3>Team Members</h3>
+            <ul>
+                <li>nathanjslim</li>
+                <li>eyobodega</li>
+                <li>StephenODea54</li>
+                <li>julianp15</li>
+                <li>jwonc4602</li>
+                <li>jpatterson933</li>
+            </ul>
         </>
     )
 }
--- client/src/components/Metrics/index.js
+++ client/src/components/Metrics/index.js
@@ -9,6 +9,7 @@ export const Metrics = () => {
         <>
             <h1>Metrics</h1>
             <p>This is where we will track the metrics of various projects by the onyx-salamander crew</p>
+            <p>This is where we can utilize the github api</p>
         </>
     )
 }
