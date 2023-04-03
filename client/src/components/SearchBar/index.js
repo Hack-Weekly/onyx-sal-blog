dbe8a2737a3ef3a0b13eb4d8f30e2fe9d80275a9
Nathan Lim <nalim@nvidia.com>
2023-04-03 12:38:58 -0400

    linting and documentation

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -5,6 +5,7 @@ import './index.css'
 // We also want to be able to search by tags. 
 
 const SearchBar = ({ onSearch }) => {
+    // Setting searchText with a default state of an empty string. 
     const [searchText, setSearchText] = useState('');
     const searchTextRef = useRef('');
 
@@ -12,16 +13,20 @@ const SearchBar = ({ onSearch }) => {
         searchTextRef.current = searchText;
     }, [searchText]);
 
+    // Ensures that the searchText state variable stays in sync with the input value as the user types. 
     const handleSearchTextChange = (event) => {
         setSearchText(event.target.value);
     };
 
+    // Setting default prop as it may return an error where onSearch is not a function. 
     SearchBar.defaultProps = {
         onSearch: () => {},
     };
 
-
+    // Since this button will presumably only be clicked once we have a string to search for, this handles our search by looking for everything that includes searchText. 
     const handleSearchButtonclick = () => {
+        // Declaring an empty array of search results. 
+
         const searchResults = [];
         const elements = document.querySelectorAll('[data-id]');
         elements.forEach((element) => {
@@ -35,6 +40,7 @@ const SearchBar = ({ onSearch }) => {
         onSearch(searchResults);
     };
 
+    // This detects the 'Enter' key and routes the function to handleSearchButtonclick.
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
             handleSearchButtonclick();
