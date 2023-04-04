1b1ce596e7f02fca3914654d9263fdecdddddb75
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-03 23:27:10 -0700

    1. Fixed api error. Added axios. Updated metrics to grab all the tags to be displayed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 // Module Imports
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Routers
 import blogRouter from "./routes/posts.js";
