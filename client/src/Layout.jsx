70894880ffec57671b6ea9474a8781e8dfd09d09
Stephen O'Dea <odeastephen1@gmail.com>
2023-04-06 14:42:03 +0000

    Add login component

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,46 +1,97 @@
+// Module Imports
+import { useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Footer } from "./components/Footer";
+
+// Global State
+import { useUserStore } from "./stores/userStore";
+
+// CSS
 import "./Layout.css";
+
+// Components
+import { Footer } from "./components/Footer";
+
+// Utils
+import { getAccessTokenGithub, getUserDataGithub } from "./utils/gitHubServices";
+
+const login = () => {
+  localStorage.setItem("loginWith", "GitHub");
+  window.location.assign(`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`);
+};
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+// Data
+const navbarLinks = [
+  {
+    id: 1,
+    path: "/",
+    label: "Blog",
+  },
+  {
+    id: 2,
+    path: "/about",
+    label: "About",
+  },
+  {
+    id: 3,
+    path: "/post",
+    label: "Post",
+  },
+  {
+    id: 4,
+    path: "/metrics",
+    label: "Metrics",
+  },
+];
+
+const Login = () => {
+  const [user, setUser] = useUserStore((state) => [state.user, state.setUser]);
+  const loginWith = useRef(localStorage.getItem("loginWith"));
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParams = urlParams.get("code");
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (codeParams && !accessToken) {
+      getAccessTokenGithub(codeParams)
+        .then(response => {
+          localStorage.setItem("accessToken", response.access_token);
+
+          getUserDataGithub(accessToken)
+            .then(response => setUser(response));
+        });
+    } else if (codeParams && accessToken) {
+      getUserDataGithub(accessToken)
+        .then(response => {
+          localStorage.setItem("accessToken", accessToken);
+          setUser(response);
+        });
+    };
+  }, [loginWith, setUser]);
+
+  return (
+    user === "" ?
+    <span className="loginBtn" onClick={() => login()}>Login</span> :
+    <img className="userLogo" src={user.avatar} alt="GitHub Logo" />
+  );
+
+};
 
 const Layout = ({ children }) => {
   const pathname = useLocation().pathname;
 
-  const navbarLinks = [
-    {
-      id: 1,
-      path: "/",
-      label: "Blog",
-    },
-    {
-      id: 2,
-      path: "/about",
-      label: "About",
-    },
-    {
-      id: 3,
-      path: "/post",
-      label: "Post",
-    },
-    {
-      id: 4,
-      path: "/metrics",
-      label: "Metrics",
-    },
-  ];
-
-
-  // Scroll to top functionality
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
   return(
     <>
 
-      
       <nav id="nav-bar">
-      <Link to = "/">
-        <h1><span>&gt; Onyx Sal Blog</span></h1>
+        <Link to = "/">
+          <h1><span>&gt; Onyx Sal Blog</span></h1>
         </Link>
         <ul>
           { navbarLinks.map(link => (
@@ -54,10 +105,13 @@ const Layout = ({ children }) => {
               <Link to={link.path}>{link.label}</Link>
             </li>
           ))}
+
+          <Login />
+
         </ul>
       </nav>
 
-      {children}
+        {children}
       {/* This is the footer component located on /component/Footer/index.js */}
       <Footer />
 
