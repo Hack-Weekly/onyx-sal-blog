22d604526407744d35b6b729b57203bcb55bd2e2
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-05 23:34:29 -0700

    1. Added bar graph for total posts each tag is in. 2. Added line graph for total Words in each post.

--- client/src/components/Metrics/index.js
+++ client/src/components/Metrics/index.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 // style sheet
 import './index.css';
 // recharts imports
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 export const Metrics = () => {
     const [tags, setTags] = useState([]);
     const [posts, setPosts] = useState([]);
@@ -13,7 +12,7 @@ export const Metrics = () => {
         axios.get('http://localhost:8000/api/posts')
             .then(response => {
                 setPosts(response.data)
-                console.log(response.data)
+                // console.log(response.data)
             })
             .catch(error => console.error(error))
         axios.get('http://localhost:8000/api/tags')
@@ -22,13 +21,40 @@ export const Metrics = () => {
             })
             .catch(error => console.error(error));
     }, []);
+    // map through our posts and create a new array
+    const wordCountData = posts.map(post => {
+        // split the content by its spaces and use reduce to get the counts for each word
+        const wordCounts = post.content.split(" ").reduce((counts, word) => {
+            // will initialize new word to 0 before adding 1, if word exists, will add 1 to it
+            counts[word] = (counts[word] || 0) + 1; // word being what we are 
+            return counts;
+        }, {});
+
+        // remove words with low counts 
+        Object.keys(wordCounts).forEach(key => {
+            if (wordCounts[key] < 5) { // currently removing words with coutn < value on right side
+                delete wordCounts[key]
+            }
+        })
+        return {
+            post: post.title,
+            wordCounts: wordCounts
+        }
+    })
+
 
     // data for bar chart, we should extract to its own module
     const tagData = tags.map(tag => ({
         name: tag.name,
-        posts: tag.posts.length
+        totalPosts: tag.posts.length
     }));
 
+    const postData = posts.map(post => ({
+        name: post.title,
+        totalWords: post.content.split(" ").length
+    }))
+
+    
     return (
         // (tags.length < 0)
         //     ?
@@ -45,8 +71,8 @@ export const Metrics = () => {
         //     </main>
         //     :
         <div id="tag-bar-graph">
-
-            <ResponsiveContainer>
+            <h1>Total Posts for Each Tag</h1>
+            <ResponsiveContainer height={230}>
                 <BarChart
                     width={500}
                     height={300}
@@ -62,11 +88,34 @@ export const Metrics = () => {
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
-                    <Legend />
-                    <Bar dataKey="posts" fill="#8884d8" />
-                    {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+                    <Legend iconSize={30} />
+                    <Bar dataKey="totalPosts" fill="#467799" />
                 </BarChart>
             </ResponsiveContainer>
+            <h1>Total Words in each post</h1>
+            <ResponsiveContainer height={230}>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={postData}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend iconSize={30} />
+                    <Line dataKey="totalWords" fill="#467799" />
+                </LineChart>
+            </ResponsiveContainer>
+            <h1>High word counts for each post</h1>
+
+
         </div>
     )
 }
