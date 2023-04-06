15f26f2fd253d5457470ee62314489541817c199
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-05 18:37:04 -0700

    1. Added style sheet to metrics component as well as made some adjustments to index.js metrics components.

--- client/src/components/Metrics/index.js
+++ client/src/components/Metrics/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+// style sheet
+import './index.css';
 
 export const Metrics = () => {
     const [tags, setTags] = useState([]);
@@ -11,17 +13,24 @@ export const Metrics = () => {
             })
             .catch(error => console.error(error));
     }, []);
+    // filter out tags with 0 posts
+    const tagsWithPosts = tags.filter(tag => tag.posts.length > 0);
+    console.log(tagsWithPosts[0], "test")
 
     return (
-        <>
-            <h1>Metrics</h1>
+        (tags.length > 0)
+        ?
+        <main id="tags-container">
             {tags && tags.map(tag => {
                 return (
-                    <h1>{tag.name}</h1>
+                    <div className="tag-card" key={tag.id}>
+                        <h1>{tag.name}</h1>
+                        {tag.posts.length > 0 ? <p>Total posts with tags {tag.posts.length}</p> : <p>This tag has no posts</p>}
+                    </div>
                 )
             })}
-            <p>This is where we will track the metrics of various projects by the onyx-salamander crew</p>
-            <p>This is where we can utilize the github api</p>
-        </>
+        </main>
+        :
+        <h1>No tags exist!</h1>
     )
 }
