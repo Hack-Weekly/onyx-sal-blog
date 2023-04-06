77abb888ee97f2130dacd70b948ac3bae59064e5
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-06 12:39:26 -0700

    1. Finished metric data for tags and posts.

--- client/src/components/Metrics/index.js
+++ client/src/components/Metrics/index.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 // style sheet
 import './index.css';
 // recharts imports
-import { BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LabelList } from 'recharts';
 export const Metrics = () => {
+    // SET STATE
     const [tags, setTags] = useState([]);
     const [posts, setPosts] = useState([]);
 
@@ -12,7 +13,6 @@ export const Metrics = () => {
         axios.get('http://localhost:8000/api/posts')
             .then(response => {
                 setPosts(response.data)
-                // console.log(response.data)
             })
             .catch(error => console.error(error))
         axios.get('http://localhost:8000/api/tags')
@@ -21,101 +21,103 @@ export const Metrics = () => {
             })
             .catch(error => console.error(error));
     }, []);
-    // map through our posts and create a new array
-    const wordCountData = posts.map(post => {
-        // split the content by its spaces and use reduce to get the counts for each word
-        const wordCounts = post.content.split(" ").reduce((counts, word) => {
-            // will initialize new word to 0 before adding 1, if word exists, will add 1 to it
-            counts[word] = (counts[word] || 0) + 1; // word being what we are 
-            return counts;
-        }, {});
-
-        // remove words with low counts 
-        Object.keys(wordCounts).forEach(key => {
-            if (wordCounts[key] < 5) { // currently removing words with coutn < value on right side
-                delete wordCounts[key]
-            }
-        })
-        return {
-            post: post.title,
-            wordCounts: wordCounts
-        }
-    })
-
 
     // data for bar chart, we should extract to its own module
     const tagData = tags.map(tag => ({
         name: tag.name,
-        totalPosts: tag.posts.length
+        totalPosts_PerTag: tag.posts.length
     }));
-
+    // calcualte total words in each post with associated post title
     const postData = posts.map(post => ({
         name: post.title,
         totalWords: post.content.split(" ").length
     }))
+    // calculate total words in existence
+    const totalWords = postData.reduce((accumulator, post) => {
+        return accumulator + post.totalWords;
+    }, 0)
+    // simple calc average function / might be cleaner way to do this
+    const calculateAverage = (total, length) => {
+        if (length === 0) { // this removed NaN warning
+            return 0;
+        }
+        const avg = total / length;
+        return avg;
+    }
+    // calculate averages for things
+    const averageWordsPerPost = calculateAverage(totalWords, postData.length)
+    const averageTagsPerPost = calculateAverage(tags.length, posts.length)
 
-    
+    // first check and make sure data is there, if so render all methods
+    // SIDE NOTE: might add an issue to modularize this data and extact it so we can resuse a bargraph component instead of rendering multiples
     return (
-        // (tags.length < 0)
-        //     ?
-        //     <main id="tags-container">
-        //         {/**Can remove tags since we already checking tags length above */}
-        //         {tags.map(tag => {
-        //             return (
-        //                 <div className="tag-card" key={tag.id}>
-        //                     <h1>{tag.name}</h1>
-        //                     {tag.posts.length > 0 ? <p>Total posts with tags {tag.posts.length}</p> : <p>This tag has no posts</p>}
-        //                 </div>
-        //             )
-        //         })}
-        //     </main>
-        //     :
-        <div id="tag-bar-graph">
-            <h1>Total Posts for Each Tag</h1>
-            <ResponsiveContainer height={230}>
-                <BarChart
-                    width={500}
-                    height={300}
-                    data={tagData}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend iconSize={30} />
-                    <Bar dataKey="totalPosts" fill="#467799" />
-                </BarChart>
-            </ResponsiveContainer>
-            <h1>Total Words in each post</h1>
-            <ResponsiveContainer height={230}>
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={postData}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend iconSize={30} />
-                    <Line dataKey="totalWords" fill="#467799" />
-                </LineChart>
-            </ResponsiveContainer>
-            <h1>High word counts for each post</h1>
+        (tags.length <= 0 || posts.length <= 0)
+            ?
+            <main id="tags-container">
+                {/**Can remove tags since we already checking tags length above */}
+                <h1>There seems to be a connection error. Please try again later.</h1>
+            </main>
+            :
+            <main>
+                <div id="post-bar-graph">
+                    <h1>Posts Metrics</h1>
+                    <p>Total Posts: <span>{posts.length}</span></p>
+                    <p style={{ opacity: 0.5 }}>Hover over data for full information</p>
 
+                    <ResponsiveContainer height={230}>
+                        <BarChart
+                            data={postData}
+                            margin={{
+                                top: 15,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis label={{ value: "Total Words", angle: -90, position: "insideLeft" }} />
+                            <Tooltip
+                                wrapperStyle={{ color: "black" }}
+                            />
+                            <Legend verticalAlign="top" height={36} iconType="square" iconSize={25} />
+                            <Bar dataKey="totalWords" fill="#467799">
+                                <LabelList dataKey="totalWords" position={"top"}></LabelList>
+                            </Bar>
+                        </BarChart>
+                    </ResponsiveContainer>
+                    <p>Total Words in existence: <span>{totalWords}</span></p>
+                    <p>Average words per post: <span>{averageWordsPerPost}</span></p>
+                </div>
+                <div id="tag-bar-graph">
+                    <h1>Tag Metrics</h1>
+                    <p>Total Tags: <span>{tags.length}</span></p>
+                    <p style={{ opacity: 0.5 }}>Hover over data for full information</p>
 
-        </div>
+                    <ResponsiveContainer height={230}>
+                        <BarChart
+                            data={tagData}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis label={{ value: "Total Posts", angle: -90, position: "insideLeft" }} />
+                            <Tooltip
+                                wrapperStyle={{ color: "black" }}
+
+                            />
+                            <Legend verticalAlign="top" height={36} iconType="square" iconSize={25} />
+                            <Bar dataKey="totalPosts_PerTag" fill="#467799" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                    <p>Total Tags in existence: <span>{tags.length}</span> </p>
+                    <p>Average Tags Per Post: <span>{averageTagsPerPost}</span> </p>
+                </div>
+            </main>
     )
 }
